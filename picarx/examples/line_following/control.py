1385a652b25cdc0c9c0441f226415b9e43e9e6cc
Evan Palmer <evanp922@gmail.com>
2023-01-20 05:51:53 +0000

    Implemented control class

--- picarx/examples/line_following/control.py
+++ picarx/examples/line_following/control.py
@@ -1,5 +1,18 @@
+import sys
+
+sys.path.append("../..")
+
+from picarx_improved import Picarx
+
 class Control:
     """Controller designed to drive the robot toward and along an identified line."""
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, car: Picarx, scale: float = 1.0) -> None:
+        self.scale = scale
+        self.car = car
+
+    def control(self, angle: float, speed: int = 30) -> None:
+        self.car.drive(speed, angle)
+
+
+    
